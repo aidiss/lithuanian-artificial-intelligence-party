6315025131e7587a1d9c3ae01a9ca27724f993bd
Aidis <aidiss@gmail.com>
2024-07-18 21:40:09 +0300

    Update completion

--- src/ldip/completion.py
+++ src/ldip/completion.py
@@ -5,9 +5,14 @@ OLLAMA_LLAMA3 = "ollama/llama3"
 OLLAMA_CHAT_LLAMA3 = "ollama_chat/llama2"
 
 GPT35_TURBO = "openai/gpt-3.5-turbo"
+GPT4o_TURBO = "openai/gpt-4o"
+
+CLAUDE35 = "anthropic/claude-3-5-sonnet-20240620"
+
+MODEL = CLAUDE35
 
 
-def complete(message, model=OLLAMA_LLAMA3):
+def complete(message, model=MODEL) -> str:
     messages = [{"role": "user", "content": message}]
     completion_response = litellm.completion(model, messages)
     content = completion_response.choices[0].message.content  # type: ignore
